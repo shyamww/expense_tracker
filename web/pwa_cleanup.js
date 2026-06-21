d1ed858115dcdc2782308f08f888192e993eb626
Shyam Kumar Gautam <shyamkumargautam@Shyams-MacBook-Air.local>
2026-06-21 16:57:54 +0530

    Clear stale Flutter web caches

--- web/pwa_cleanup.js
+++ web/pwa_cleanup.js
@@ -1,10 +1,37 @@
 (function () {
-  if (!('serviceWorker' in navigator)) return;
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    registrations.forEach(function (registration) {
-      registration.unregister();
-    });
+  var reloadKey = 'expense_tracker_pwa_cleanup_reloaded';
+  var hadController = !!(navigator.serviceWorker && navigator.serviceWorker.controller);
+  var tasks = [];
+
+  if ('serviceWorker' in navigator) {
+    tasks.push(
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        return Promise.all(registrations.map(function (registration) {
+          return registration.unregister();
+        }));
+      })
+    );
+  }
+
+  if ('caches' in window) {
+    tasks.push(
+      caches.keys().then(function (keys) {
+        return Promise.all(keys
+          .filter(function (key) {
+            return key.indexOf('flutter-') === 0;
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          }));
+      })
+    );
+  }
+
+  Promise.all(tasks).then(function () {
+    if (!hadController || sessionStorage.getItem(reloadKey) === '1') return;
+    sessionStorage.setItem(reloadKey, '1');
+    window.location.reload();
   }).catch(function () {
-    // The app must still boot if a browser blocks service-worker access.
+    // The app must still boot if a browser blocks service-worker/cache access.
   });
 })();
